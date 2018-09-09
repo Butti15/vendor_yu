f48e2e904400367a9d23dc8e2d5cf226baf47564
Prashant Maheshwari <prashantqwerty4@gmail.com>
2018-09-09 16:11:22 +0530

    Fix My Derp !

--- tomato/Android.mk
+++ tomato/Android.mk
@@ -35,7 +35,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_api_v02
 LOCAL_MODULE_OWNER := yu
 LOCAL_SRC_FILES := proprietary/vendor/lib64/libloc_api_v02.so
-LOCAL_MULTILIB := both
+LOCAL_MULTILIB := 64
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -44,7 +44,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libwpa_qmi_eap_proxy
 LOCAL_MODULE_OWNER := yu
-LOCAL_SRC_FILES := proprietary/vendor/lib64/libwpa_qmi_eap_proxy.so
+LOCAL_SRC_FILES := proprietary/lib64/libwpa_qmi_eap_proxy.so
 LOCAL_MULTILIB := 64
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
